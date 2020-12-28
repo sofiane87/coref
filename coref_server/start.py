0920cb748baf11238fb5a92c9d1d90fbcd2b4b99
sofiane87 <sofiane-mahiou@hotmail.fr>
2020-12-28 22:25:21 +0100

    Add jquery & ajax

--- coref_server/start.py
+++ coref_server/start.py
@@ -1,36 +1,23 @@
+from pprint import PrettyPrinter
+import json
 import flask
-from flask import request, jsonify
+import os
+from flask import request, jsonify, render_template
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+pp = PrettyPrinter(indent=1)
 
-
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
-
-    # Create an empty list for our results
-    results = []
-
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
-
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    return jsonify(results)
+@app.route('/analyse', methods=['GET'])
+def analyse():
+    result = json.loads(request.args.get("result"))
+    pp.pprint(result)
+    return {"text": "Hello"}
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return render_template("index.jinja")
+
 
 app.run()
---
+++ coref_server/static/form.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    var counter = 0;
+
+    $("#addrow").on("click", function () {
+        counter++;
+        var newField = $('<div class="form-row align-items-center" id="Expression' + counter + '">');
+        var cols = '<div class="col-auto"> <input name="expression_' + counter + '" id="inlineFormInput' + counter + '" class="form-control mb-2" type="text" placeholder="Expression"/></div>';
+        cols += '<div class="col-auto"><div class="form-check mb-2"><input name="case_sensitive_' + counter + '" id="autoSizingCheck' + counter + '" class="form-check-input" type="checkbox"/> <label class="form-check-label" for="autoSizingCheck' + counter + '"> Case sensitive </label>  </div></div>';
+        cols += '<div class="col-auto"><button class="btn btn-info mb-2" id="BtnDelete' + counter +'">Delete</button></div>';
+        newField.append(cols);
+        $("#ExpressionForm").append(newField);
+
+        var local_counter = counter
+
+        $("#BtnDelete" + local_counter).on("click", function () {
+            $("#Expression" + local_counter).remove();
+        });
+    });
+
+
+    $("#analyse_btn").on("click", function() {
+      var target_text = $("textarea#TextToAnalyse").val();
+      var form_fields = $("form#ExpressionForm").serializeToJSON();
+      console.log({"text": target_text, "form": form_fields});
+        $.ajax({
+            "url": "/analyse",
+            "type": "GET",
+            "dataType": "json",
+            "data": {"result": JSON.stringify({"text": target_text, "expressions": form_fields})},
+            "success": function(result) {
+                console.log(result)
+            },
+            error: function(xhr, resp, text) {
+                    console.log(xhr, resp, text);
+                }
+        })
+    })
+
+
+});
